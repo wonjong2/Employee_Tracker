dc4eb98556e93adeb60efb6995dbbbe8489ab3fe
Wonjong Park <wonjong2@gmail.com>
2022-05-10 14:30:42 -0700

    Implemented the function for 'Add An Employee'

--- server.js
+++ server.js
@@ -28,43 +28,44 @@ const options = [
 ];
 
 function main() {
-    inquirer.prompt(
-        [
-            {
-                type: 'rawlist',
-                name: 'option',
-                message: "What would you like to do?",
-                choices: options,
-                pageSize: 7
+    inquirer
+        .prompt(
+            [
+                {
+                    type: 'rawlist',
+                    name: 'option',
+                    message: "What would you like to do?",
+                    choices: options,
+                    pageSize: 7
+                }
+            ]
+        )
+        .then(({option}) => {
+            switch(options.indexOf(option)) {
+                case 0:
+                    viewDepartment();
+                    break;
+                case 1:
+                    viewRole();
+                    break;
+                case 2:
+                    viewEmployee();
+                    break;
+                case 3:
+                    addDepartment();
+                    break;
+                case 4:
+                    addRole();
+                    break;
+                case 5:
+                    addEmployee();
+                    break;
+                case 6:
+                    // function1();
+                    break;
             }
-        ]
-    )
-    .then(({option}) => {
-        switch(options.indexOf(option)) {
-            case 0:
-                viewDepartment();
-                break;
-            case 1:
-                viewRole();
-                break;
-            case 2:
-                viewEmployee();
-                break;
-            case 3:
-                addDepartment();
-                break;
-            case 4:
-                addRole();
-                break;
-            case 5:
-                addEmployee();
-                break;
-            case 6:
-                // function1();
-                break;
-        }
-    })
-    .catch(err => console.error(err));
+        })
+        .catch(err => console.error(err));
 }
 
 const viewDepartment = async () => {
@@ -126,21 +127,22 @@ const viewEmployee = async () => {
 }
 
 const addDepartment = async() => {
-    inquirer.prompt(
-        [
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the name of the department?',
-            }
-        ]
-    )
-    .then(async ({name}) => {
-        const result = await db.query(`INSERT INTO department(name) VALUES('${name}')`);
-        console.log(`Added ${name} to the database`);
-        main();
-    })
-    .catch((err) => console.log(err));
+    inquirer
+        .prompt(
+            [
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is the name of the department?',
+                }
+            ]
+        )
+        .then(async ({name}) => {
+            const result = await db.query(`INSERT INTO department(name) VALUES('${name}')`);
+            console.log(`Added ${name} to the database`);
+            main();
+        })
+        .catch((err) => console.log(err));
 };
 
 const addRole = async() => {
@@ -154,33 +156,34 @@ const addRole = async() => {
         return;
     }
 
-    inquirer.prompt(
-        [
-            {
-                type: 'input',
-                name: 'title',
-                message: 'What is the name of the role?',
-            },
-            {
-                type: 'input',
-                name: 'salary',
-                message: 'What is the salary of the role?',
-            },
-            {
-                type: 'list',
-                name: 'department_id',
-                message: 'Which department does the role belong to?',
-                choices: deptList,
-                pageSize: deptList.length,
-            }
-        ]
-    )
-    .then(async ({title, salary, department_id}) => {
-        const result = await db.query(`INSERT INTO role(title, salary, department_id) VALUES('${title}', ${salary}, ${department_id})`);
-        console.log(`Added ${title} to the database`);
-        main();
-    })
-    .catch((err) => console.log(err));
+    inquirer
+        .prompt(
+            [
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'What is the name of the role?',
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'What is the salary of the role?',
+                },
+                {
+                    type: 'list',
+                    name: 'department_id',
+                    message: 'Which department does the role belong to?',
+                    choices: deptList,
+                    pageSize: deptList.length,
+                }
+            ]
+        )
+        .then(async ({title, salary, department_id}) => {
+            const result = await db.query(`INSERT INTO role(title, salary, department_id) VALUES('${title}', ${salary}, ${department_id})`);
+            console.log(`Added ${title} to the database`);
+            main();
+        })
+        .catch((err) => console.log(err));
 };
 
 const addEmployee = async () => {
@@ -192,50 +195,47 @@ const addEmployee = async () => {
         employeeList = await db.query(`SELECT id AS value, first_name AS name, last_name FROM employee`);
         employeeList = employeeList[0];
         employeeList.forEach((employee) => employee.name = employee.name + ' ' + employee.last_name);
-        console.log(roleList, employeeList);
+        employeeList.unshift({value:null, name: 'None'});
     }
     catch {
         console.error(`Error in Reading DB`);
         return;
     }
 
-    inquirer.prompt(
-        [
-            {
-                type: 'input',
-                name: 'first_name',
-                message: `What is the employee's first name?`,
-            },
-            {
-                type: 'input',
-                name: 'last_name',
-                message: `What is the employee's last name?`,
-            },
-            {
-                type: 'list',
-                name: 'role',
-                message: `What is the employee's role?`,
-                choices: roleList,
-                pageSize: roleList.length,
-            },
-            {
-                type: 'list',
-                name: 'manager',
-                message: `Who is the employee's manager?`,
-                choices: employeeList,
-                pageSize: employeeList.length,
-            }
-        ]
-    )
-    .then(async ({first_name, last_name, role, manager}) => {
-        console.log(first_name, last_name, role, manager);
-        // const deptId = deptList[deptList.findIndex(findDeptId)].id;
-        // function findDeptId(row) {
-        //     return row.name === department;
-        // }
-        // const result = await db.query(`INSERT INTO role(title, salary, department_id) VALUES('${title}', ${salary}, ${deptId})`);
-        // console.log(`Added ${title} to the database`);
-        main();
-    })
-    .catch((err) => console.log(err));
+    inquirer
+        .prompt(
+            [
+                {
+                    type: 'input',
+                    name: 'first_name',
+                    message: `What is the employee's first name?`,
+                },
+                {
+                    type: 'input',
+                    name: 'last_name',
+                    message: `What is the employee's last name?`,
+                },
+                {
+                    type: 'list',
+                    name: 'role_id',
+                    message: `What is the employee's role?`,
+                    choices: roleList,
+                    pageSize: roleList.length,
+                },
+                {
+                    type: 'list',
+                    name: 'manager_id',
+                    message: `Who is the employee's manager?`,
+                    choices: employeeList,
+                    pageSize: employeeList.length,
+                }
+            ]
+        )
+        .then(async ({first_name, last_name, role_id, manager_id}) => {
+            console.log(first_name, last_name, role_id, manager_id);
+            const result = await db.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('${first_name}', '${last_name}', ${role_id}, ${manager_id})`);
+            console.log(`Added ${first_name} ${last_name} to the database`);
+            main();
+        })
+        .catch((err) => console.log(err));
 }
