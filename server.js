fed4bec177f36d4737b442977a215927dfe65f6b
Wonjong Park <wonjong2@gmail.com>
2022-05-13 20:07:36 -0700

    Private information related to DB is separated into .evn file and use dotenv package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('console.table');
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 const inquirer = require('inquirer');
 const {Views, viewData, viewEmployeesByManager, viewEmployeesByDept, viewUtilizedBudget} = require('./functions/view')
@@ -11,9 +12,9 @@ let db = {};
 mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'Ajtwlssha123!',
-        database: 'employee_db'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 )
 .then((conn) => {
