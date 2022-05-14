348740153a9be72503a10f797097867605406f70
Wonjong Park <wonjong2@gmail.com>
2022-05-13 20:00:36 -0700

    Updated .gitignore and schema.sql files

--- db/schema.sql
+++ db/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE employee (
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE CASCADE,
+    ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
